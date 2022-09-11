d612aa21fc700009da4a85bf0f9a172b3da64bce
AnastasiaSergeeva05 <112942778+AnastasiaSergeeva05@users.noreply.github.com>
2022-09-11 20:43:43 +0300

    Update and rename src/Main.java to источник/Main.java

---
+++ источник/Main.java
@@ -0,0 +1,70 @@
+public class Main {
+    public static void main(String[] args) {
+        // Задача 1
+int clientOs = 1;
+int iOS = 0;
+int android = 1;
+if (clientOs == iOS){
+System.out.println("Установите версию приложения iOS");
+}else
+System.out.println("Установите версию приложения Android");
+
+// Задача 2
+
+int clientDeviceYear = 2015;
+int aNdroid = 1;
+int iOs = 0;
+int clientOS = 1;
+if (clientOS<clientDeviceYear && clientOS == android){
+System.out.println("Установите облегченную версию приложения Android");
+} else{
+System.out.println("Установите  версию приложения Android");
+}  if (clientOS<clientDeviceYear && clientOS == iOs){
+System.out.println("Установите облегченную версию приложения iOs");
+} else
+System.out.println("Установите версию приложения iOs");
+//Задача3
+int year = 2021;
+if (year %100==0 && year %400 ==0 )
+System.out.println(year+"является высокосным");
+else if (year % 4 == 0 && year % 100 > 0)
+System.out.println(year+"является высокосным");
+else if (year % 100 == 0)
+System.out.println(year+"не является высокосным");
+else
+System.out.println(year+"не является высокосным");
+
+// Задача4
+int deliveryDistance = 95;
+int deliveryDay = 1;
+if (deliveryDistance<=20) {
+System.out.println("Потребуется дней: " + deliveryDay);
+}else if (deliveryDistance>20 && deliveryDistance <=60 ){
+System.out.println("Потребуется дней: " + deliveryDay*2);
+}else if (deliveryDistance>60 && deliveryDistance <=100){
+System.out.println("Потребуется дней: " + deliveryDay*3);
+}else
+System.out.println("Потребуется дней больше 3 " );
+
+// Задача5
+int monthNumber = 10;
+switch (monthNumber){
+case 12:
+case 1:
+case 2:
+System.out.println("Зима");
+case 3:
+case 4:
+case 5:
+System.out.println("Весна");
+case 6:
+case 7:
+case 8:
+System.out.println("Лето");
+case 9:
+case 10:
+case 11:
+System.out.println("Осень");
+}
+    }
+}
